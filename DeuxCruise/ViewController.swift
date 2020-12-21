d5592f262be19aca8069047a752930c2561723a1
1Soyebo <soyeboibukun@gmail.com>
2020-12-21 15:53:12 +0100

    created project

---
+++ DeuxCruise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeuxCruise
+//
+//  Created by Ibukunoluwa Soyebo on 21/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
